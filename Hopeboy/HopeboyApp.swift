a4bad55e46e4ad4b895f43922d527ab628dae5be
hoffmann-jan <34597495+hoffmann-jan@users.noreply.github.com>
2022-12-21 11:52:59 +0100

    Initial Commit

---
+++ Hopeboy/HopeboyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopeboyApp.swift
+//  Hopeboy
+//
+//  Created by jhope on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HopeboyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
